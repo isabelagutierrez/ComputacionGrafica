518b898e994ccc0fdfd65276a1cc65e198872610
Isabela <isabelagutierrez@javerianacali.edu.co>
2025-02-08 09:25:53 -0500

    Archivos

--- CODIGO/CODIGO/Cubo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Cubo.h"
-void Cubo::dibujarCubo(float x, float y, float z, float red, float green, float blue, float solid, float angulo, float x1, float y1, float z1) {
-    glPushMatrix();
-        glColor3f(red, green, blue);
-        glTranslatef(x, y, z);
-        glRotatef(angulo,x1,y1,z1);
-        glutSolidCube(solid);
-    glPopMatrix();
-}
--- CODIGO/CODIGO/Tetera.cpp
+++
@@ -1,9 +0,0 @@
-#include "Tetera.h"
-void Tetera::dibujarTetera(float x, float y, float z, float red, float green, float blue, float solid) {
-    glPushMatrix();
-        glColor3f(red, green, blue);
-        glTranslatef(x, y, z);
-        glutSolidTeapot(solid);
-        glEnd();
-    glPopMatrix();
-}
